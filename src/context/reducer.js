3396d165b1db2c7ab9eda9d8249adb1ea5cf2940
the22mastermind <bertrandmasabo22@gmail.com>
2020-11-06 16:32:09 +0200

    added-donation-page

---
+++ src/context/reducer.js
@@ -0,0 +1,13 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_DONATION':
+      return {
+        ...state,
+        data: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
